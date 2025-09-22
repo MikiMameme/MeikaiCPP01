685b5736b83ea537a388bd4578e464abdbe0a564
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-22 20:13:45 +0900

    01-03_01-07_変数

---
+++ 01-03_01-07_変数/01-03_01-07_変数.cpp
@@ -0,0 +1,19 @@
+﻿//2つの変数の合計と平均を表示
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	int y;
+
+	x = 63;
+	y = 18;
+
+	cout << "xの値は" << x << "です\n";
+	cout << "yの値は" << y << "です\n";
+	cout << "合計は" << x + y << "です\n";
+	cout << "平均は" << (x + y) / 2 << "です\n";
+}
