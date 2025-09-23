25135d53a24de1fc357c5992104b53394095c9bb
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-24 01:39:40 +0900

    01-03_01-09_変数と初期化2

---
+++ 01-03_01-09_変数と初期化2/01-03_01-09_変数と初期化2.cpp
@@ -0,0 +1,15 @@
+﻿//2つの変数の合計と平均を表示
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int x(63); //C++は
+    int y{18}; //こんな書き方でもOK
+    cout << "xの値は" << x << "です\n";
+    cout << "yの値は" << y << "です\n";
+    cout << "合計は" << x + y << "です\n";
+    cout << "平均は" << (x + y) / 2 << "です\n";
+}
