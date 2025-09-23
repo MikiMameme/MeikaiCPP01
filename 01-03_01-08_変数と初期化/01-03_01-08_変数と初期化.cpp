4b8c9aa3388c4f8da5351b437072023fcd61208a
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-24 01:39:09 +0900

    01-03_01-08_変数と初期化
    コンパイルエラー

---
+++ 01-03_01-08_変数と初期化/01-03_01-08_変数と初期化.cpp
@@ -0,0 +1,15 @@
+﻿//2つの変数の合計と平均を表示
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x; //これはVisualStudioだとエラーになる
+	int y; //不定値にしてはいけない！
+
+	cout << "xの値は" << x << "です\n";
+	cout << "yの値は" << y << "です\n";
+	cout << "平均は" << (x + y) / 2 << "です\n";
+}
