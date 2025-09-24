535f5e90c0cfef982ac02e986d46acafef7282bc
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-24 21:05:59 +0900

    01-04_01-12_単項の算術演算子

---
+++ 01-04_01-12_単項の算術演算子/01-04_01-12_単項の算術演算子.cpp
@@ -0,0 +1,16 @@
+﻿//整数値を読み込んで符号を反転した値を表示
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+
+	cout << "整数値 :";
+	cin >> a;
+
+	int b = -a;
+	cout << a << "の符号を反転した値は" << b << "です\n";
+}
