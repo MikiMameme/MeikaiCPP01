ce942c341155e6dc546c653877c1bfada9a9099e
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-25 00:40:37 +0900

    01-04_01-18_文字列の読込み

---
+++ 01-04_01-18_文字列の読込み/01-04_01-18_文字列の読込み.cpp
@@ -0,0 +1,16 @@
+﻿//文字を読み込んで表示
+
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	string name;
+
+	cout << "文字を入力してください :";
+	cin >> name;
+
+	cout << "こんにちは" << name << "さん\n";
+}
