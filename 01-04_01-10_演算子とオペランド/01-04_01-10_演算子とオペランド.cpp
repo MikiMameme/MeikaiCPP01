db1db5c6e18fbf69d68321ae3ea78fa53c0d238d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-24 20:22:53 +0900

    01-04_01-10_演算子とオペランド

---
+++ 01-04_01-10_演算子とオペランド/01-04_01-10_演算子とオペランド.cpp
@@ -0,0 +1,25 @@
+﻿//2つの整数値を読み込んで加減乗除した値を表示
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	int y;
+
+	cout << "xとyを加減乗除します\n";
+
+	cout << "xの値 :";
+	cin >> x;
+
+	cout << "yの値 :";
+	cin >> y;
+
+	cout << "x + yは" << x + y << "です\n";
+	cout << "x - yは" << x - y << "です\n";
+	cout << "x * yは" << x * y << "です\n";
+	cout << "x / yは" << x / y << "です\n";
+	cout << "x % yは" << x % y << "です\n";
+}
