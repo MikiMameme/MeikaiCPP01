39eb68b5613be952039fdcc3003bb4cc404d3441
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-24 21:09:11 +0900

    01-04_01-13_実数値を読み込み

---
+++ 01-04_01-13_実数値を読み込み/01-04_01-13_実数値を読み込み.cpp
@@ -0,0 +1,24 @@
+﻿//2つの実数値を読み込んで加減乗除した値を表示
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	double x;
+	double y;
+
+	cout << "xとyを加減乗除します\n";
+
+	cout << "xの値 :";
+	cin >> x;
+
+	cout << "yの値 :";
+	cin >> y;
+
+	cout << "x + yは" << x + y << "です\n";
+	cout << "x - yは" << x - y << "です\n";
+	cout << "x * yは" << x * y << "です\n";
+	cout << "x / yは" << x / y << "です\n";
+}
