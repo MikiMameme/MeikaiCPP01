62b998d1218d36ab8dfc14acfb53d319223ff2ac
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-25 00:05:28 +0900

    01-04_01-16_乱数の生成

---
+++ 01-04_01-16_乱数の生成/01-04_01-16_乱数の生成.cpp
@@ -0,0 +1,16 @@
+﻿//０～９のラッキーナンバーを乱数で生成
+
+#include<ctime>
+#include<cstdlib>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	int lucky = rand() % 10;
+
+	cout << "今日のラッキーナンバーは" << lucky << "です\n";
+}
