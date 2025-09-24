9c018cc817cdefaad0c8f57bf9e849207739a4c8
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-25 00:41:26 +0900

    01-04_01-20_文字列の読込み3

---
+++ 01-04_01-20_文字列の読込み3/01-04_01-20_文字列の読込み3.cpp
@@ -0,0 +1,16 @@
+﻿//文字を読み込んで表示（スペースも読み込む）
+
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	string s1 = "ABC";
+	string s2 = "XYZ";
+	s1 = "FBI"; //s1を代入して書き換える
+
+	cout << "文字列s1は" << s1 << "です\n";
+	cout << "文字列s2は" << s2 << "です\n";
+}
