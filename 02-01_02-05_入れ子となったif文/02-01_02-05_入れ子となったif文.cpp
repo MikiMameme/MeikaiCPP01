c44fa80aa33374659233d826a563ef72c517600d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-25 19:05:53 +0900

    02-01_02-05_入れ子となったif文

---
+++ 02-01_02-05_入れ子となったif文/02-01_02-05_入れ子となったif文.cpp
@@ -0,0 +1,20 @@
+﻿//読み込んだ整数値の符号を判定
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cout << "整数値　:";
+	cin >> n;
+
+	if (n > 0)
+		cout << "その値は正です\n";
+	else if (n < 0)
+		cout << "その値は負です\n";
+	else
+		cout << "その値は０です\n";
+}
