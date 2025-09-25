69b21ba12bfaaa3475ce7751c42ae76b801b3d7a
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-25 19:06:14 +0900

    02-01_02-06_入れ子となったif文2

---
+++ 02-01_02-06_入れ子となったif文2/02-01_02-06_入れ子となったif文2.cpp
@@ -0,0 +1,21 @@
+﻿//読み込んだ整数値が奇数偶数か判定
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cout << "整数値　:";
+	cin >> n;
+
+	if (n > 0)
+		if(n % 2 == 0)
+		cout << "その値は偶数です\n";
+		else
+			cout << "その値は奇数です\n";
+	else
+		cout << "\a正しくない値が入力されました！\n";
+}
