568ad2c74965e7f4acb3e457239dc1967a6f6ee4
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-26 17:58:36 +0900

    02-01_02-07_論理演算子

---
+++ 02-01_02-07_論理演算子/02-01_02-07_論理演算子.cpp
@@ -0,0 +1,20 @@
+﻿//読み込んだ整数値の桁数を判定
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cout << "整数値 :";
+	cin >> n;
+
+	if (n == 0)
+		cout << "その値はゼロです\n";
+	else if (n >= -9 && n <= 9)
+		cout << "その値は1桁です\n";
+	else
+		cout << "その値は2桁以上です\n";
+}
