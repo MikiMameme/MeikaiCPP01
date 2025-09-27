d56e440af6ad49704daaf211a27ebe902753ff38
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-27 18:23:55 +0900

    02-01_02-16_条件における宣言2

---
+++ 02-01_02-16_条件における宣言2/02-01_02-16_条件における宣言2.cpp
@@ -0,0 +1,22 @@
+﻿//条件における宣言2
+
+#include<cmath>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	double x;
+
+	cout << "実数値 :";
+	cin >> x;
+
+	if (double m = fmod(x, 10)) {
+		cout << "その値は10では割り切れません\n";
+		cout << "最下位桁は" << m << "です\n";
+	}
+	else {
+		cout << "その値は10で割り切れます\n";
+	}
+}
