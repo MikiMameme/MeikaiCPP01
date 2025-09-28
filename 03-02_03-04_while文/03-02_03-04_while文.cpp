412e27e0113cb01694b2f525fa3e1957f9e05367
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-28 17:13:18 +0900

    03-02_03-04_while文

---
+++ 03-02_03-04_while文/03-02_03-04_while文.cpp
@@ -0,0 +1,21 @@
+﻿//正の整数値を０までカウントダウン
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+
+	cout << "カウントダウンします\n";
+	do {
+		cout << "正の整数値を入力してください :";
+		cin >> x;
+	} while (x <= 0);
+
+	while (x >= 0) {
+		cout << x << "\n";
+		x--;
+	}
+}
