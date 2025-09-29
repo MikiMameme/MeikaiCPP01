b01f60c0e63202d3eb8966d92a3d641c60955d5e
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-29 17:20:07 +0900

    03-03_03-10_for文

---
+++ 03-03_03-10_for文/03-03_03-10_for文.cpp
@@ -0,0 +1,16 @@
+﻿//読み込んだ個数だけ表示（for文）
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "何個表示しますか :";
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cout << '*';
+	cout << '\n';
+}
