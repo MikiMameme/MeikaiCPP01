39704e7bcd77e28c0b192b1539fb6fa3f9b84fe9
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-02 01:13:26 +0900

    04-01_04-09_bool型2

---
+++ 04-01_04-09_bool型2/04-01_04-09_bool型2.cpp
@@ -0,0 +1,24 @@
+﻿//bool型の値を表示
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "整数a , b :";
+	cin >> a >> b;
+
+	cout << boolalpha;
+
+	cout << "a < b =" << (a < b) << '\n';
+	cout << "a <= b =" << (a <= b) << '\n';
+	cout << "a > b =" << (a > b) << '\n';
+	cout << "a >= b =" << (a >= b) << '\n';
+	cout << "a == b =" << (a == b) << '\n';
+	cout << "a != b =" << (a != b) << '\n';
+	cout << "!a     =" << (!a) << '\n';
+	cout << "!b     =" << (!b) << '\n';
+
+}
