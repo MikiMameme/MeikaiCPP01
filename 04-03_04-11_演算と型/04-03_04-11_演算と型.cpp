4ea4e5674201b529723112c8265373e3eff90949
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-02 01:21:06 +0900

    04-03_04-11_演算と型

---
+++ 04-03_04-11_演算と型/04-03_04-11_演算と型.cpp
@@ -0,0 +1,17 @@
+﻿//２つの整数値の平均値を求める
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y;
+
+	cout << "２つの整数値xとyの平均値を求めます\n";
+	cout << "xの値 :"; cin >> x;
+	cout << "yの値 :"; cin >> y;
+
+	double ave = (x + y) / 2;
+	cout << "xとyの平均値は" << ave << "です\n";
+}
