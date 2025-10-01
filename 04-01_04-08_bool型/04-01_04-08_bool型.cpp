21a77cd98c2f521360d195311f96f5e27785fbf8
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-02 00:45:21 +0900

    04-01_04-08_bool型

---
+++ 04-01_04-08_bool型/04-01_04-08_bool型.cpp
@@ -0,0 +1,16 @@
+﻿//bool型の値を表示
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << true << ' ' << false << '\n';
+
+	cout << boolalpha;
+	cout << true << ' ' << false << '\n';
+
+	cout << noboolalpha;
+	cout << true << ' ' << false << '\n';
+}
