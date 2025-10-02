a00cda56ca9185e331d45aea081e6b992a70aa3f
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-02 17:45:41 +0900

    04-03_04-15_繰り返しの制御

---
+++ 04-03_04-15_繰り返しの制御/04-03_04-15_繰り返しの制御.cpp
@@ -0,0 +1,17 @@
+﻿//繰り返しをfloatで制御
+
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+	float sum = 0.0F;
+	cout << fixed << setprecision(6);
+	for (float x = 0.0F; x <= 1.0F; x += 0.001F) {
+		cout << "x =" << x << '\n';
+		sum += x;
+	}
+	cout << "sum =" << sum << '\n';
+}
