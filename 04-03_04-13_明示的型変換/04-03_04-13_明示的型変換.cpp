b504a6d606c2ad2263b225c93ed8fdaa51189b00
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-02 17:21:37 +0900

    04-03_04-13_明示的型変換

---
+++ 04-03_04-13_明示的型変換/04-03_04-13_明示的型変換.cpp
@@ -0,0 +1,17 @@
+﻿//2つの整数値の平均値を実数値として求める（その１）
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y;
+
+	cout << "2つの整数値xとyの平均値を求めます\n";
+	cout << "xの値 :"; cin >> x;
+	cout << "yの値 :"; cin >> y;
+
+	double ave = (x + y) / 2.0;
+	cout << "xとyの平均値は" << ave << "です\n";
+}
