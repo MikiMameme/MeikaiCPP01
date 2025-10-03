b5a504c5d600a6f4f098ffd7e0532f2186a73368
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-03 19:47:41 +0900

    05-01_05-08_定値オブジェクトで表す要素数2

---
+++ 05-01_05-08_定値オブジェクトで表す要素数2/05-01_05-08_定値オブジェクトで表す要素数2.cpp
@@ -0,0 +1,20 @@
+﻿//配列の全要素に乱数を代入して表示
+
+#include<ctime>
+#include<cstdlib>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	const int n = 5;
+	int a[n];
+
+	for (int i = 0; i < n; i++)
+		a[i] = rand() % 10;
+
+	for (int i = 0; i < n; i++)
+		cout << "a[" << i << "]=" << a[i] << '\n';
+}
