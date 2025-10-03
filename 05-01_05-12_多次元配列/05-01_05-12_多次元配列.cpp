2d2abec9adc10bdde23d18e18b6ae565670db04e
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-03 20:49:24 +0900

    05-01_05-12_多次元配列

---
+++ 05-01_05-12_多次元配列/05-01_05-12_多次元配列.cpp
@@ -0,0 +1,23 @@
+﻿//全校生要素の値を読み込んで表示
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int m[3][2];
+
+	cout << "各要素の値を代入せよ\n";
+	for(int i = 0; i < 3; i++){
+		for (int j = 0; j < 2; j++) {
+			cout << "m[" << i << "][" << j << "]:";
+			cin >> m[i][j];
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 2; j++) {
+			cout << "m[" << i << "][" << j << "]:" << m[i][j] << '\n';
+		}
+	}
+}
