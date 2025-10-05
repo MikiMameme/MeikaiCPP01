255a9ed9e37d7dce5e2f57e795aefd7371d63874
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-05 20:14:55 +0900

    05-01_05-15_多次元配列型の情報の取得

---
+++ 05-01_05-15_多次元配列型の情報の取得/05-01_05-15_多次元配列型の情報の取得.cpp
@@ -0,0 +1,17 @@
+﻿//多次元配列の型と要素数を表示
+
+#include<iostream>
+#include<typeinfo>
+
+using namespace std;
+
+int main()
+{
+	int a[5][4];
+	double b[4][2][3];
+
+	cout << "配列aの型 :" << typeid(a).name() << '\n';
+	cout << "aの要素数 :" << typeid(a[0]).name() << '\n';
+	cout << "配列bの型 :" << typeid(b).name() << '\n';
+	cout << "bの要素型 :" << typeid(b[0]).name() << '\n';
+}
