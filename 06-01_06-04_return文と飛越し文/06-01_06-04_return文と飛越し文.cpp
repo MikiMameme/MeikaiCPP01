6aef30fb7298e348bc07a2a8d64338219d3191f6
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-07 19:38:56 +0900

    06-01_06-04_return文と飛越し文

---
+++ 06-01_06-04_return文と飛越し文/06-01_06-04_return文と飛越し文.cpp
@@ -0,0 +1,24 @@
+﻿//２つの最大値を求めて表示
+
+#include<iostream>
+
+using namespace std;
+
+//最大値を返却
+int max(int a, int b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+}
+
+int main()
+{
+	int a, b, c;
+
+	cout << "整数a :"; cin >> a;
+	cout << "整数b :"; cin >> b;
+
+	cout << "最大値は" << max(a, b) << "です\n";
+}
