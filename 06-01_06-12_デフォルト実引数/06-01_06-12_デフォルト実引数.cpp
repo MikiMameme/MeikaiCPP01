e1a19f2ef42768e64982afcbb9dd5a2d503ac15b
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-08 21:07:32 +0900

    06-01_06-12_デフォルト実引数

---
+++ 06-01_06-12_デフォルト実引数/06-01_06-12_デフォルト実引数.cpp
@@ -0,0 +1,21 @@
+﻿//アラートを発する関数(デフォルト実引数)
+
+#include<iostream>
+
+using namespace std;
+
+//n回アラートを発する
+void alerts(int n = 3)
+{
+	while (n-- > 0)
+		cout << '\a';
+}
+
+int main()
+{
+	alerts();
+	cout << "アラート！\n";
+
+	alerts(5);
+	cout << "再びアラート！\n";
+}
