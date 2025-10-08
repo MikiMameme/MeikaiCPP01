e9a763e7ebe2e11aa6c5b0162bf6fa591ebfe058
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-08 21:05:33 +0900

    06-01_06-11_引数を受け取らない関数

---
+++ 06-01_06-11_引数を受け取らない関数/06-01_06-11_引数を受け取らない関数.cpp
@@ -0,0 +1,39 @@
+﻿//暗算トレーニング
+
+#include<ctime>
+#include<cstdlib>
+#include<iostream>
+
+using namespace std;
+
+//続行の確認
+bool confirm_retry()
+{
+	int retry;
+	do {
+		cout << "もう一度しますか <Yes--1 / No--0> :";
+		cin >> retry;
+	} while (retry != 0 && retry != 1);
+	return static_cast<bool>(retry);
+}
+
+int main()
+{
+	srand(time(NULL));
+	cout << "暗算トレーニングを始めます\n";
+
+	do {
+		int x = rand() % 900 + 100;
+		int y = rand() % 900 + 100;
+		int z = rand() % 900 + 100;
+
+		while (true) {
+			int k;
+			cout << x << " + " << y << " + " << z << " = ";
+			cin >> k;
+			if (k == x + y + z)
+				break;
+			cout << "\a違います\n";
+		}
+	} while (confirm_retry());
+}
