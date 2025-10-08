bb2c49defb082a9c2ce256ea5bea5353343e4e50
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-08 21:47:35 +0900

    06-02_06-15_参照渡し

---
+++ 06-02_06-15_参照渡し/06-02_06-15_参照渡し.cpp
@@ -0,0 +1,28 @@
+﻿//2つの引数の値を交換する
+
+#include<iostream>
+
+using namespace std;
+
+//引数と値を交換
+void swap(int& x, int& y)
+{
+	int t = x;
+	x = y;
+	y = t;
+}
+
+int main()
+{
+	int a, b;
+
+	cout << "変数a :"; cin >> a;
+	cout << "変数b :"; cin >> b;
+
+	//aとbを交換
+	swap(a, b);
+
+	cout << "変数aとbの価を交換しました\n";
+	cout << "変数aの価は" << a << "です\n";
+	cout << "変数bの価は" << b << "です\n";
+}
