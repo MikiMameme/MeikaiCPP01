f41522271f1c118270630af55c1820da5a9d42ac
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-08 21:14:15 +0900

    06-02_06-13_値渡しの限界

---
+++ 06-02_06-13_値渡しの限界/06-02_06-13_値渡しの限界.cpp
@@ -0,0 +1,28 @@
+﻿//2つの引数の値を交換する（間違い）
+
+#include<iostream>
+
+using namespace std;
+
+//引数と値を交換（間違い）
+void swap(int x, int y)
+{
+	int t = x;
+	x = y;
+	y = t;
+}
+
+int main()
+{
+	int a, b;
+
+	cout << "変数a :"; cin >> a;
+	cout << "変数b :"; cin >> b;
+
+	//aとbを交換？？
+	swap(a, b);
+
+	cout << "変数aとbの価を交換しました\n";
+	cout << "変数aの価は" << a << "です\n";
+	cout << "変数bの価は" << b << "です\n";
+}
