744e7c87e8c9ca44c64050fe7a30d60b6195900a
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-08 20:39:27 +0900

    06-01_06-09_関数の汎用性

---
+++ 06-01_06-09_関数の汎用性/06-01_06-09_関数の汎用性.cpp
@@ -0,0 +1,27 @@
+﻿//直角三角形を表示（関数版）
+
+#include<iostream>
+
+using namespace std;
+
+//*をn個連続表示
+void put_nchar(int n, char c)
+{
+	while (n-- > 0)
+		cout << c;
+}
+
+int main()
+{
+	int n;
+
+	cout << "右下直角の三角形を表示します\n";
+	cout << "段数は :";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		put_nchar(n - i, ' ');
+		put_nchar(i, '+');
+		cout << '\n';
+	}
+}
