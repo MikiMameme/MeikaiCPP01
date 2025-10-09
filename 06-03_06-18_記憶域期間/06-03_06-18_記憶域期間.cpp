5449c295f5f1ea056aa262a35ca487aa6b3cff47
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-09 17:33:14 +0900

    06-03_06-18_記憶域期間

---
+++ 06-03_06-18_記憶域期間/06-03_06-18_記憶域期間.cpp
@@ -0,0 +1,21 @@
+﻿//自動記憶域期間と静的記憶域期間
+
+#include<iostream>
+
+using namespace std;
+
+//静的記憶域期間（０で初期化）
+int fx;
+
+int main()
+{
+	//静的記憶域期間（０で初期化）
+	static int sx;
+
+	//自動記憶域期間（不定値で初期化したかったがVisualStudioではコンパイルエラーになった）
+	int ax;
+
+	cout << "ax = " << ax << '\n';
+	cout << "sx = " << sx << '\n';
+	cout << "fx = " << fx << '\n';
+}
