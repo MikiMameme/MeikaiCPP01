3e4b711349f8491c6faf41b567adab2c851bd133
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-09 18:18:39 +0900

    06-03_06-21_参照を返却する関数2

---
+++ 06-03_06-21_参照を返却する関数2/06-03_06-21_参照を返却する関数2.cpp
@@ -0,0 +1,23 @@
+﻿//配列の要素への参照を返却する関数
+
+#include<iostream>
+
+using namespace std;
+
+const int a_size = 5;
+
+
+int& r(int idx)
+{
+	static int a[a_size];
+	return a[idx];
+}
+
+int main()
+{
+	for (int i = 0; i < a_size; i++)
+		r(i) = i;
+
+	for (int i = 0; i < a_size; i++)
+		cout << "r(" << i << ") =" << r(i) << '\n';
+}
