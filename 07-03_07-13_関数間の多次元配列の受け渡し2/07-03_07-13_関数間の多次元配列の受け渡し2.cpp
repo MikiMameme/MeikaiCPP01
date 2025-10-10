fe153e8925247eb0422ba4ab17c7c260b5f94c95
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-10 20:18:55 +0900

    07-03_07-13_関数間の多次元配列の受け渡し2

---
+++ 07-03_07-13_関数間の多次元配列の受け渡し2/07-03_07-13_関数間の多次元配列の受け渡し2.cpp
@@ -0,0 +1,24 @@
+﻿//typeid演算子によって表示
+
+#include<iostream>
+#include<typeinfo>
+
+using namespace std;
+
+//要素型とする配列
+void func(int a[][3])
+{
+	cout << "仮引数aの型 :" << typeid(a).name() << '\n';
+}
+
+int main(void)
+{
+	int x[2][3];
+	int y[4][3];
+
+	cout << "実引数xの型 :" << typeid(x).name() << '\n';
+	func(x);
+
+	cout << "実引数yの型 :" << typeid(y).name() << '\n';
+	func(y);
+}
