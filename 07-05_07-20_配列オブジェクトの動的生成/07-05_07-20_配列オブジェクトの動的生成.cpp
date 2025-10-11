1a208b7b9e239998aac702879ce3f3d86a47ce9d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-11 19:06:45 +0900

    07-05_07-20_配列オブジェクトの動的生成

---
+++ 07-05_07-20_配列オブジェクトの動的生成/07-05_07-20_配列オブジェクトの動的生成.cpp
@@ -0,0 +1,21 @@
+﻿//整数配列オブジェクトを動的に生成
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int asize;
+	cout << "要素数:";
+	cin >> asize;
+
+	int* a = new int[asize];
+	
+	for (int i = 0; i < asize; i++)
+		a[i] = i;
+
+	for (int i = 0; i < asize; i++)
+		cout << "a[" << i << "]=" << a[i] << '\n';
+	delete[] a;
+}
