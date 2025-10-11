01557f617088266dd0c801b5e11d8c85dff04a96
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-11 18:23:00 +0900

    07-04_07-16_線形探索

---
+++ 07-04_07-16_線形探索/07-04_07-16_線形探索.cpp
@@ -0,0 +1,35 @@
+﻿//線形探索（その１）
+
+#include<iostream>
+
+using namespace std;
+
+//値keyを線形探索
+int seq_search(int* a, int n, int key)
+{
+	for (int i = 0; i < n; i++)
+		if (*a++ == key)
+			return i;
+	return -1;
+}
+
+int main(void)
+{
+	int key, idx;
+	int x[8];
+	int x_size = sizeof(x) / sizeof(x[0]);
+
+	for (int i = 0; i < x_size; i++) {
+		cout << "x[" << i << "]: ";
+		cin >> x[i];
+	}
+
+	cout << "探す値は :";
+	cin >> key;
+
+	if ((idx = seq_search(x, x_size, key)) != -1)
+		cout << "その値を持つ要素はx[" << idx << "]です\n";
+
+	else
+		cout << "見つかりませんでした\n";
+}
