ce9b433c22c0e4bfe7917b460085c57580a970e4
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-11 18:21:06 +0900

    07-04_07-14_ポインタによる配列要素の走査

---
+++ 07-04_07-14_ポインタによる配列要素の走査/07-04_07-14_ポインタによる配列要素の走査.cpp
@@ -0,0 +1,30 @@
+﻿//配列の全要素に０を代入（その１）
+
+#include<iostream>
+
+using namespace std;
+
+//要素に０を代入
+void fill_zero(int* p, int n)
+{
+	while (n-- > 0) {
+		*p = 0;
+		p++;
+	}
+}
+
+int main(void)
+{
+	int x[5] = { 1, 2, 3, 4, 5 };
+	int x_size = sizeof(x) / sizeof(x[0]);
+
+	for (int i = 0; i < x_size; i++)
+		cout << "x[" << i << "] =" << x[i] << '\n';
+
+	fill_zero(x, x_size);
+
+	cout << "全要素に０を代入しました\n";
+
+	for (int i = 0; i < x_size; i++)
+		cout << "x[" << i << "] =" << x[i] << '\n';
+}
