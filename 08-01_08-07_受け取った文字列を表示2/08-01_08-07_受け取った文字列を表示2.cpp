bc66005012cea73e46b360f97451c13f8f541638
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-13 01:09:04 +0900

    08-01_08-07_受け取った文字列を表示2

---
+++ 08-01_08-07_受け取った文字列を表示2/08-01_08-07_受け取った文字列を表示2.cpp
@@ -0,0 +1,24 @@
+﻿//受け取った文字列中の小文字を大文字に変換して表示
+
+#include<iostream>
+#include<cctype>
+
+using namespace std;
+
+//文字列を表示
+void put_upper(const char s[])
+{
+	for (int i = 0; s[i]; i++)
+		cout << static_cast <char>(toupper(s[i]));
+}
+
+int main()
+{
+	char str[36];
+
+	cout << "文字列 :";
+	cin >> str;
+
+	put_upper(str);
+	cout << '\n';
+}
