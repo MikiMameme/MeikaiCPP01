39fc377d7fed8546a7e02c7b2d9ebdd364f46d88
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-12 17:26:03 +0900

    08-01_08-05_キーボードからの文字列の読み込み

---
+++ 08-01_08-05_キーボードからの文字列の読み込み/08-01_08-05_キーボードからの文字列の読み込み.cpp
@@ -0,0 +1,15 @@
+﻿//名前を尋ねて表示（文字列の読み込みと表示）
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char name[36];
+
+	cout << "お名前は:";
+	cin >> name;
+
+	cout << "こんにちは、" << name << "さん\n";
+}
