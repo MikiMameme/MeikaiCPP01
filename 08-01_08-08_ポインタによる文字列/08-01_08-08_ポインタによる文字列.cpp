340047e51373829708227eda3c68d55594b6b31f
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-13 01:09:25 +0900

    08-01_08-08_ポインタによる文字列

---
+++ 08-01_08-08_ポインタによる文字列/08-01_08-08_ポインタによる文字列.cpp
@@ -0,0 +1,14 @@
+﻿//配列による文字列とポインタによる文字列
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char str[] = "ABC";
+	const char* ptr = "XYZ";
+
+	cout << "str = \"" << str << "\"\n";
+	cout << "ptr = \"" << ptr << "\"\n";
+}
